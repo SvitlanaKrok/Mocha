12d66ab0a19f829715d32ef6bba9faa3e100b9ae
Svitlana Krokhmalna <sveta.fetsyk@gmail.com>
2023-02-05 20:39:53 +0200

    correction changes regarding remarks

--- cypress/e2e/HomeWorkMochaBeforeEach.cy.js
+++ cypress/e2e/HomeWorkMochaBeforeEach.cy.js
@@ -10,36 +10,24 @@ describe('Inline form', () => {
      })
 
 describe('Name field', () => {     
-  const fieldName = [
-        {name: 'Svitlana', expectedName: 'Svitlana'},
-        {name: 'SVITLANA', expectedName: 'SVITLANA'},
-        {name: 'Ira', expectedName: 'Ira'},
-        {name: 'SvitlanaKrohmalna', expectedName: 'SvitlanaKrohmalna'}
+  const fieldNameEmail = [
+        {name: 'Svitlana', expectedName: 'Svitlana', email: 'sveta.fetsyk@gmail.com', expectedEmail: 'sveta.fetsyk@gmail.com'},
+        {name: 'SVITLANA', expectedName: 'SVITLANA', email: 'svetaFetsyk@gmail.com', expectedEmail: 'svetaFetsyk@gmail.com'},
+        {name: 'Ira', expectedName: 'Ira', email: 'ira123@gmail.com', expectedEmail: 'ira123@gmail.com'},
+        {name: 'SvitlanaKrohmalna', expectedName: 'SvitlanaKrohmalna', email: 'sveta_123@gmail.com', expectedEmail: 'sveta_123@gmail.com'}
     ]
-    fieldName.forEach(({name, expectedName}) => {
-        it(`Search Name Data ${name}`, () => {
+    fieldNameEmail.forEach(({name, expectedName,email, expectedEmail}) => {
+        it(`Search Name and Email Data ${name}, ${email}`, () => {
            
             cy.get('input[type="text"][placeholder="Jane Doe"]', { timeout: 60000 }).type(`${name}`);
-            cy.get('input[type="text"][placeholder="Jane Doe"]').should("contain.value", `${expectedName}`);
-        })
-   })        
- })
-
- describe('Email field', () => {     
-    const fieldEmail = [
-        {email: 'sveta.fetsyk@gmail.com', expectedEmail: 'sveta.fetsyk@gmail.com'},
-        {email: 'svetaFetsyk@gmail.com', expectedEmail: 'svetaFetsyk@gmail.com'},
-        {email: 'sveta123@gmail.com', expectedEmail: 'sveta123@gmail.com'},
-        {email: 'sveta_123@gmail.com', expectedEmail: 'sveta_123@gmail.com'}
-    ]
-    fieldEmail.forEach(({email, expectedEmail}) => {
-        it(`Search Name Data ${email}`, () => {
-          
+            cy.get('input[type="text"][placeholder="Jane Doe"]').should("contain.value", `${expectedName}`);       
             cy.get('form > input[placeholder="Email"]', { timeout: 60000 }).type(`${email}`);
             cy.get('form > input[placeholder="Email"]').should("contain.value", `${expectedEmail}`);
         })
    })        
  })
+
+
 })
 
 
